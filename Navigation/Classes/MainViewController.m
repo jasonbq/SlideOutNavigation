be83c86969760456a00acbfd48bdb5674c6f3532
Jason Cheung <jasonbq@gmail.com>
2013-04-16 02:02:31 +0800

    add slide left and right panels.

--- Navigation/Classes/MainViewController.m
+++ Navigation/Classes/MainViewController.m
@@ -10,16 +10,23 @@
 #import "CenterViewController.h"
 #import "LeftPanelViewController.h"
 #import "RightPanelViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 #define CENTER_TAG 1
-#define LEFT_TAG 2
+#define LEFT_PANEL_TAG 2
 #define RIGHT_TAG 3
+#define CORNER_RADIUS 4
+#define SLIDE_TIMING .25
+#define PANEL_WIDTH 63
+#define RIGHT_PANEL_TAG 3
 
 
 @interface MainViewController () <CenterViewControllerDelegate>
 @property (strong, nonatomic) CenterViewController *centerViewController;
 @property (strong, nonatomic) LeftPanelViewController *leftPanelViewController;
+@property (strong, nonatomic) RightPanelViewController *rightPanelViewController;
 @property (assign, nonatomic) BOOL showingLeftPanel;
+@property (assign, nonatomic) BOOL showingRightPanel;
 @end
 
 @implementation MainViewController
@@ -82,17 +89,41 @@
 
 - (void)showCenterViewWithShadow:(BOOL)value withOffset:(double)offset
 {
+    if (value) {
+        self.centerViewController.view.layer.cornerRadius = CORNER_RADIUS;
+        self.centerViewController.view.layer.shadowColor = [[UIColor blackColor] CGColor];
+        self.centerViewController.view.layer.shadowOpacity = 0.8;
+        self.centerViewController.view.layer.shadowOffset = CGSizeMake(offset, offset);
+    }else{
+        self.centerViewController.view.layer.cornerRadius = 0;
+        self.centerViewController.view.layer.shadowOffset = CGSizeMake(offset, offset);
+    }
 }
 
 - (void)resetMainView
 {
+    if (self.leftPanelViewController != nil) {
+        [self.leftPanelViewController.view removeFromSuperview];
+        self.leftPanelViewController = nil;
+        
+        self.centerViewController.leftButton.tag = 1;
+        self.showingLeftPanel = NO;
+    }
+    if (self.rightPanelViewController != nil) {
+        [self.rightPanelViewController.view removeFromSuperview];
+        self.rightPanelViewController = nil;
+        
+        self.centerViewController.rightButton.tag = 1;
+        self.showingRightPanel = NO;
+    }
+    [self showCenterViewWithShadow:NO withOffset:0];
 }
 
 - (UIView *)getLeftView
 {
     if (self.leftPanelViewController == nil) {
         self.leftPanelViewController = [[LeftPanelViewController alloc] initWithNibName:@"LeftPanelViewController" bundle:nil];
-        self.leftPanelViewController.view.tag = LEFT_TAG;
+        self.leftPanelViewController.view.tag = LEFT_PANEL_TAG;
         self.leftPanelViewController.delegate = self.centerViewController;
         
         [self.view addSubview:self.leftPanelViewController.view];
@@ -107,8 +138,20 @@
 }
 
 - (UIView *)getRightView
-{     
-    UIView *view = nil;
+{
+    if (self.rightPanelViewController == nil) {
+        self.rightPanelViewController = [[RightPanelViewController alloc] initWithNibName:@"RightPanelViewController" bundle:nil];
+        self.rightPanelViewController.view.tag = RIGHT_TAG;
+        self.rightPanelViewController.delegate = self.centerViewController;
+        
+        [self.view addSubview:self.rightPanelViewController.view];
+        [self addChildViewController:self.rightPanelViewController];
+        [self.rightPanelViewController didMoveToParentViewController:self];
+        self.rightPanelViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    }
+    self.showingRightPanel = YES;
+    [self showCenterViewWithShadow:YES withOffset:2];
+    UIView *view = self.rightPanelViewController.view;
     return view;
 }
 
@@ -119,6 +162,7 @@
 
 - (void)setupGestures
 {
+    
 }
 
 -(void)movePanel:(id)sender
@@ -130,14 +174,41 @@
 
 - (void)movePanelLeft // to show right panel
 {
+    UIView *childView = [self getRightView];
+    [self.view sendSubviewToBack:childView];
+    
+    [UIView animateWithDuration:SLIDE_TIMING delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        self.centerViewController.view.frame = CGRectMake(-self.view.frame.size.width + PANEL_WIDTH, 0, self.view.frame.size.width, self.view.frame.size.height);
+    } completion:^(BOOL finished) {
+        if (finished) {
+            self.centerViewController.rightButton.tag = 0;
+        }
+    }];
 }
 
 - (void)movePanelRight // to show left panel
 {
+    UIView *childView = [self getLeftView];
+    [self.view sendSubviewToBack:childView];
+    
+    [UIView animateWithDuration:SLIDE_TIMING delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        self.centerViewController.view.frame = CGRectMake(self.view.frame.size.width - PANEL_WIDTH, 0, self.view.frame.size.width, self.view.frame.size.height);
+    } completion:^(BOOL finished) {
+        if (finished) {
+            self.centerViewController.leftButton.tag = 0;
+        }
+    }];
 }
 
 - (void)movePanelToOriginalPosition
-{    
+{
+    [UIView animateWithDuration:SLIDE_TIMING delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        self.centerViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    } completion:^(BOOL finished){
+        if (finished) {
+            [self resetMainView];
+        }
+    }];
 }
 
 #pragma mark -
